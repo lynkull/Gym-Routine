8f87b28770659914a87b42fdb609e0dbe0b97d80
Ed Cres <crespoed07@gmail.com>
2020-02-24 02:56:14 -0500

    better layout, setting up adapter

--- app/src/main/java/com/aldreduser/gymroutine/mainScreenRecycler/Adapter.kt
+++ app/src/main/java/com/aldreduser/gymroutine/mainScreenRecycler/Adapter.kt
@@ -1,4 +1,19 @@
 package com.aldreduser.gymroutine.mainScreenRecycler
 
-class Adapter {
+import android.content.Context
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class Adapter(val nContext: Context, val sets:ArrayList<Int>, val reps:ArrayList<Int>, val weight:ArrayList<Double>): RecyclerView.Adapter<Adapter.viewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Adapter.viewHolder {
+    }
+
+    override fun getItemCount(): Int {
+    }
+
+    override fun onBindViewHolder(holder: Adapter.viewHolder, position: Int) {
+    }
+
+    public class viewHolder(itemView: View): RecyclerView.ViewHolder(itemView)
 }
