15d6f86c65be7d1ae200de903b687cb7952af17b
Ed Cres <crespoed07@gmail.com>
2020-02-24 20:48:28 -0500

    better layout, implementing adapter

--- app/src/main/java/com/aldreduser/gymroutine/MainActivity.kt
+++ app/src/main/java/com/aldreduser/gymroutine/MainActivity.kt
@@ -2,6 +2,9 @@ package com.aldreduser.gymroutine
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.aldreduser.gymroutine.mainScreenRecycler.Adapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 //have a navigation bar to the left (says which workout day)
 //recyclerview says what to display in the main screen
@@ -26,9 +29,54 @@ have simple undo functionality after user input
 // sets 4 and up are 'gone' by default (should be shown if the user has more than 3 sets in their workout)
 
 class MainActivity : AppCompatActivity() {
+    private var workoutName:ArrayList<String> = ArrayList()
+    private var set1Reps:ArrayList<Int> = ArrayList()
+    private var set1Weight:ArrayList<Double> = ArrayList()
+    private var set2Reps:ArrayList<Int> = ArrayList()
+    private var set2Weight:ArrayList<Double> = ArrayList()
+    private var set3Reps:ArrayList<Int> = ArrayList()
+    private var set3Weight:ArrayList<Double> = ArrayList()
+    private var set4Reps:ArrayList<Int> = ArrayList()
+    private var set4Weight:ArrayList<Double> = ArrayList()
+    private var set5Reps:ArrayList<Int> = ArrayList()
+    private var set5Weight:ArrayList<Double> = ArrayList()
+    private var set6Reps:ArrayList<Int> = ArrayList()
+    private var set6Weight:ArrayList<Double> = ArrayList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        loadData()
+        // todo: maybe put items in an arraylist inside another arraylist to make the code cleaner, to avoid so many variables
+        val adapter = Adapter(this) // need to send the rest of the arraylists
+
+        mainRecycler.layoutManager = LinearLayoutManager(this)
+        mainRecycler.adapter = adapter
+    }
+
+    private fun loadData() {
+        itemsToLoad()
+        itemsToLoad()
+        itemsToLoad()
+        itemsToLoad()
+        itemsToLoad()
+    }
+
+    // make data added by user input
+    private fun itemsToLoad() {
+        workoutName.add("Incline Dumbell")
+        set1Reps.add(3)
+        set1Weight.add(25.toDouble())
+        set2Reps.add(3)
+        set2Weight.add(30.toDouble())
+        set3Reps.add(3)
+        set3Weight.add(35.toDouble())
+        set4Reps.add(3)
+        set4Weight.add(35.toDouble())
+        set5Reps.add(3)
+        set5Weight.add(35.toDouble())
+        set6Reps.add(3)
+        set6Weight.add(35.toDouble())
     }
 }
--- app/src/main/java/com/aldreduser/gymroutine/mainScreenRecycler/Adapter.kt
+++ app/src/main/java/com/aldreduser/gymroutine/mainScreenRecycler/Adapter.kt
@@ -1,18 +1,35 @@
 package com.aldreduser.gymroutine.mainScreenRecycler
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.aldreduser.gymroutine.R
 
-class Adapter(val nContext: Context, val sets:ArrayList<Int>, val reps:ArrayList<Int>, val weight:ArrayList<Double>): RecyclerView.Adapter<Adapter.viewHolder>() {
+class Adapter(val nContext: Context,
+              val specificWorkout:ArrayList<String>,
+
+
+
+
+
+
+              val reps:ArrayList<Int>,
+              val weight:ArrayList<Double>): RecyclerView.Adapter<Adapter.viewHolder>() {
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Adapter.viewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val callForRow = inflater.inflate(R.layout.extra_recycler_main_item, parent, false)
+        return viewHolder(callForRow)
     }
 
     override fun getItemCount(): Int {
+        return specificWorkout.size
     }
 
     override fun onBindViewHolder(holder: Adapter.viewHolder, position: Int) {
+
     }
 
     public class viewHolder(itemView: View): RecyclerView.ViewHolder(itemView)
