d8f8ef11b77ce6f977e60f5efdd529517d6a643f
Ed Cres <crespoed07@gmail.com>
2020-04-22 23:35:11 -0400

    data is saved to memory

--- app/src/main/java/com/aldreduser/gymroutine/mainScreen/MainActivity.kt
+++ app/src/main/java/com/aldreduser/gymroutine/mainScreen/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.aldreduser.gymroutine.mainScreen
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -10,6 +11,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 /** todo:
  * soon:
+ * - when a new view is created, user has to input the name of the workout in a popup
  * - user can add workouts (might not work yet)
  * - store user input in boxes
  * - have a navigation bar to the left (says which workout day, nutrition, maxes)
@@ -36,14 +38,21 @@ import kotlinx.android.synthetic.main.activity_main.*
 //sets 4 and up are 'gone' by default (should be shown if the user has more than 3 sets in their workout)
 
 class MainActivity : AppCompatActivity(), MainAdapter.OnSaveButListener {
-    // maybe put items in an arraylist inside another arraylist to make the code cleaner, to avoid so many variables
+
+    //Shared Preferences Keys ("$position $key")
+    private val NUMB_OF_RECYCLER_VIEWS_SPK = "Number of recycler views"
+    private val SET_REPS_SPK = "Reps of set"        //"$position $SET_REPS_SPK $numOfSet"
+    private val SET_WEIGHT_SPK = "Weight of set"
+
     private var workoutNames:ArrayList<String> = ArrayList()
-    private var set1Reps:ArrayList<Int> = ArrayList(); private var set1Weight:ArrayList<Double> = ArrayList()
-    private var set2Reps:ArrayList<Int> = ArrayList(); private var set2Weight:ArrayList<Double> = ArrayList()
-    private var set3Reps:ArrayList<Int> = ArrayList(); private var set3Weight:ArrayList<Double> = ArrayList()
-    private var set4Reps:ArrayList<Int> = ArrayList(); private var set4Weight:ArrayList<Double> = ArrayList()
-    private var set5Reps:ArrayList<Int> = ArrayList(); private var set5Weight:ArrayList<Double> = ArrayList()
-    private var set6Reps:ArrayList<Int> = ArrayList(); private var set6Weight:ArrayList<Double> = ArrayList()
+
+// maybe put items in an arrayList inside another arrayList to make the code cleaner, to avoid so many variables
+//    private var set1Reps:ArrayList<Int> = ArrayList(); private var set1Weight:ArrayList<Double> = ArrayList()
+//    private var set2Reps:ArrayList<Int> = ArrayList(); private var set2Weight:ArrayList<Double> = ArrayList()
+//    private var set3Reps:ArrayList<Int> = ArrayList(); private var set3Weight:ArrayList<Double> = ArrayList()
+//    private var set4Reps:ArrayList<Int> = ArrayList(); private var set4Weight:ArrayList<Double> = ArrayList()
+//    private var set5Reps:ArrayList<Int> = ArrayList(); private var set5Weight:ArrayList<Double> = ArrayList()
+//    private var set6Reps:ArrayList<Int> = ArrayList(); private var set6Weight:ArrayList<Double> = ArrayList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -74,16 +83,39 @@ class MainActivity : AppCompatActivity(), MainAdapter.OnSaveButListener {
     }
 
     //todo: handle save data onLick events from MainAdapter here (look at best practice way click listener youtube video)
-    override fun onSaveClick(position: Int) {
-        //todo: use clicklisteners here
+    override fun onSaveClick(position: Int, setReps: ArrayList<String>, setWeight: ArrayList<String>) {
+        //todo: handle click events here
         //pass all the data to be saved from the adapter to here as parameters. Save the under the position
+
+        //todo: setReps.size and setWeight.size might be bugs, hopefully it starts at 0 and ends in setReps.size-1
+        for (i in 0..setReps.size) {
+            //save
+            val numOfSet = i+1
+            val repsSharedPref = this.getPreferences(Context.MODE_PRIVATE) ?: return
+            with(repsSharedPref.edit()) {
+                putString("$position $SET_REPS_SPK $numOfSet", setReps[i])
+                commit()
+            }
+        }
+        for (i in 0..setWeight.size) {
+            //save
+            val numOfSet = i+1
+            val weightSharedPref = this.getPreferences(Context.MODE_PRIVATE) ?: return
+            with(weightSharedPref.edit()) {
+                putString("$position $SET_WEIGHT_SPK $numOfSet", setWeight[i])
+                commit()
+            }
+        }
+
+        //addRecyclerData()   //display the date from memory (maybe)
     }
 
 
+    //todo: load the saved data like this
 //    // make data be added by user input
 //    private fun addRecyclerData() {
 //        addSetButton.setOnClickListener { set4Line.visibility = View.VISIBLE }
-//        workoutName.add("Incline Dumbell")
+//        workoutName.add("Incline Dumbbell")
 //        set1Reps.add(3)
 //        set1Weight.add(25.toDouble())
 //        set2Reps.add(3)
@@ -98,3 +130,6 @@ class MainActivity : AppCompatActivity(), MainAdapter.OnSaveButListener {
 //        set6Weight.add(35.toDouble())
 //    }
 }
+
+//val thisActSharedPref = this.getPreferences(Context.MODE_PRIVATE) ?: return
+//textBoxToSave.setText(thisActSharedPref.getString(prefFileName, ""))
